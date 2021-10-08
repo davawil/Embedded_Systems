71c1b209fd0b843f91690c2557dbc7d5b7d3459b
David Wilkins <wilkins.david@hotmail.com>
2021-10-08 15:05:35 +0200

    delay function

---
+++ Lab1_0/main.c
@@ -0,0 +1,15 @@
+#include "msp.h"
+
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	//configure clock
+	//CS->KEY = CS_KEY_VAL;
+
+	//configure IO
+}
